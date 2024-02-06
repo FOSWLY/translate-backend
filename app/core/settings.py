7aea797c5be87d89b0b400309ca289f08041f140
Toil <toil.official@yandex.com>
2024-02-06 14:24:01 +0300

    added hyperdx & log rotating settings

--- app/core/settings.py
+++ app/core/settings.py
@@ -15,14 +15,20 @@ class Settings(BaseSettings):
     log_level: int = logging.INFO # level of logs (DEBUG, INFO, WARNING, ERROR, CRITICAL)
     log_save: bool = True # save logs to file
     log_rich_formatter: bool = True # format logs with rich lib
+    log_max_size: int = 10 # maximum log size in megabytes
+    log_max_saved_log: int = 7 # count of saved log from running instance
+
+    # - hyperdx telemetry -
+    hyperdx_api_key: str = "" # hyperdx.io
+    hyperdx_service_name: str = "foswly-translate-backend"
 
     # - app section -
     app_name: str = '[FOSWLY] Translate'
     app_desc: str = '[FOSWLY] Translate is Free Yandex Translate API without any authorization or restrictions.'
-    app_version: str = '1.0.2'
+    app_version: str = '1.0.3'
     app_license: str = 'MIT'
     app_developer_url: str = 'https://github.com/FOSWLY/translate-backend'
-    app_developer_email: str = 'toil.contact@yandex.com'
+    app_developer_email: str = 'me@toil.cc'
 
     model_config = SettingsConfigDict(env_file='.env')
 
