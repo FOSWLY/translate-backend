89cad25e1eee4d5994a721f7153ed279f5a75dc2
Toil <toil.official@yandex.com>
2024-05-09 01:17:24 +0300

    deleted old code

--- app/routes/__init__.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-from routes.endpoints import docs, health, translate
-
-api_router = APIRouter()
-api_router.include_router(docs.router)
-api_router.include_router(translate.router, tags = ['Translate'])
-api_router.include_router(health.router, tags = ['Health'])
