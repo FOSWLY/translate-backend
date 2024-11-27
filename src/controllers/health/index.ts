e49abceb7c0bc90453d9f859d51e2d23cbe5f00e
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-11-27 14:35:17 +0300

    added health response model

--- src/controllers/health/index.ts
+++ src/controllers/health/index.ts
@@ -1,18 +1,22 @@
 import { Elysia } from "elysia";
 
 import config from "../../config";
+import { HealthResponse } from "@/models/health.model";
 
 export default new Elysia().group("/health", (app) =>
   app.get(
     "/",
     () => ({
       version: config.app.version,
-      status: "ok",
+      status: "ok" as const,
     }),
     {
       detail: {
         summary: "Get health",
         tags: ["Health"],
+      },
+      response: {
+        200: HealthResponse,
       },
     },
   ),
