da1e5d1e97855423a43ffd8363b4971870101ef3
Toil <62353659+ilyhalight@users.noreply.github.com>
2025-04-14 18:26:49 +0300

    chore: pino -> @vaylo/pino

--- src/logging.ts
+++ src/logging.ts
@@ -1,64 +1,15 @@
-import * as path from "node:path";
-import { pino, type TransportMultiOptions, type TransportTargetOptions } from "pino";
-
 import config from "@/config";
+import { PinoClient } from "@vaylo/pino";
 
-const { loki } = config.logging;
-const startingDate = new Date().toISOString().split("T")[0];
+const {
+  logging: { loki, level, logPath, logToFile },
+} = config;
 
-type PinoOpts = Parameters<typeof pino>[0] & {
-  transport: TransportMultiOptions & { targets: TransportTargetOptions[] };
-};
-
-// https://github.com/pinojs/pino/issues/1791
-// if don't take out the options separately, it willn't work
-const opts: PinoOpts = {
-  level: config.logging.level,
-  redact: {
-    // these just cause clutter
-    paths: ["pid", "hostname"],
-    remove: true,
-  },
-  transport: {
-    targets: [],
-  },
-};
-
-// it may be higher level than global, but it cann't be lower
-opts.transport.targets.push({
-  level: config.logging.level,
-  target: "pino-pretty",
-  options: {
-    colorized: true,
-  },
+export const loggerClient = new PinoClient({
+  loki,
+  level,
+  logToFile,
+  logPath,
 });
 
-opts.transport.targets.push({
-  level: config.logging.level,
-  target: "pino/file",
-  options: {
-    destination: path.join(config.logging.logPath, `${startingDate}.log`),
-  },
-});
-
-if (loki.host) {
-  opts.transport.targets.push({
-    level: config.logging.level,
-    target: "pino-loki",
-    options: {
-      batching: true,
-      interval: 5,
-      labels: { application: config.logging.loki.label },
-      host: loki.host,
-      basicAuth:
-        loki.user && loki.password
-          ? {
-              username: loki.user,
-              password: loki.password,
-            }
-          : undefined,
-    },
-  });
-}
-
-export const log = pino(opts);
+export const log = loggerClient.init();
